74b425b325bfbd7954470a0512f87fdf037de989
idncod <idncod@protonmail.com>
2026-02-22 18:18:32 +0000

    feat: add test starter in cypress

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -16,14 +16,23 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-import { mount } from 'cypress/react'
+import { mount } from 'cypress/react';
 
 // Augment the Cypress namespace to include type definitions for
 // your custom command.
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
 
-Cypress.Commands.add('mount', mount)
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            mount: typeof mount;
+        }
+    }
+}
 
+Cypress.Commands.add('mount', mount);
+
+export {};
 // Example use:
 // cy.mount(<MyComponent />)
