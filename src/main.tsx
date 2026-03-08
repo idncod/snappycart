7e94463a458d1ff6663521407a0d20a38f4c9864
idncod <idncod@protonmail.com>
2026-03-08 17:37:21 +0000

    feat: add demo for snappyart on the landing

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { CartProvider } from './cart/context/CartProvider';
-import './cart/components/snappycart.scss';
+import '../src/globals.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
