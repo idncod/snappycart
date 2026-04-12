cc99c6d517cfdf3d6a00bf5bb9f9dc069e71575a
zina6916 <191519830+zina6916@users.noreply.github.com>
2026-04-12 13:31:39 -0400

    Fix Open Drawer button locator and add assertion for Your Cart title visibility (#41)
    Co-authored-by: Zinaida <zmileshin@gmail.com>

--- playwright/e2e/cart-drawer-open-close.spec.ts
+++ playwright/e2e/cart-drawer-open-close.spec.ts
@@ -4,12 +4,13 @@ test('should open and close cart drawer', async ({ page }) => {
   await page.goto('/');
 
   const drawer = page.getByRole('dialog');
-  const openButton =  page.locator('button:has-text("Open drawer")').first();
+  const drawerTitle = page.getByRole('dialog').getByRole('heading', {name: 'Your Cart'});
+  const openDrawerButton =  page.locator('._miniGrid_1jydu_133').getByRole('button', { name: 'Open drawer' })
   const closeButton = page.getByRole('button', { name: 'Close cart' });
   await expect(drawer).toBeHidden();
-  await openButton.click();
+  await openDrawerButton.click();
   await expect(drawer).toBeVisible();
-
+  await expect(drawerTitle).toBeVisible();
   await closeButton.click();
   await expect(drawer).toBeHidden();
 });
