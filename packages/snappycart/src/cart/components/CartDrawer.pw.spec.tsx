b8d4e7899eb39d27f09249d5be98b1db098fee23
Tetiana <ztv1128@gmail.com>
2026-05-19 11:18:08 -0400

    component test added CartDrawer does not render when closed. Added test-ct script

--- packages/snappycart/src/cart/components/CartDrawer.pw.spec.tsx
+++ packages/snappycart/src/cart/components/CartDrawer.pw.spec.tsx
@@ -20,4 +20,14 @@ test.describe('CartDrawer', () => {
     await component.locator('[data-cy="cart-close"]').click();
     expect(closed).toBe(true);
   });
+
+  test('CartDrawer does not render when closed', async ({ mount }) => {
+    const component = await mount(
+      <CartProvider>
+        <CartDrawer open={false} onClose={() => {}} title="Your Cart" />
+      </CartProvider>,
+    );
+
+    await expect(component.locator('[data-testid="cart-drawer"]')).not.toBeVisible();
+  });
 });
