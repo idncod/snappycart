0a89f8a787ef9896260fc0db6445eb8052f46a34
Tetiana <ztv1128@gmail.com>
2026-05-31 01:09:02 -0400

    update: data-testid added to first test, updated the second test with mountDrawer

--- packages/snappycart/src/cart/components/CartDrawer.pw.spec.tsx
+++ packages/snappycart/src/cart/components/CartDrawer.pw.spec.tsx
@@ -32,18 +32,15 @@ test.describe('CartDrawer', () => {
         closed = true;
       },
     });
-    await expect(component.locator('[data-cy="cart-drawer"]')).toBeVisible();
-    await component.locator('[data-cy="cart-close"]').click();
+    await expect(component.locator('[data-testid="cart-drawer"]')).toBeVisible();
+    await component.locator('[data-testid="cart-close"]').click();
     expect(closed).toBe(true);
   });
 
   test('CartDrawer does not render when closed', async ({ mount }) => {
-    const component = await mount(
-      <CartProvider>
-        <CartDrawer open={false} onClose={() => {}} title="Your Cart" />
-      </CartProvider>,
-    );
-
+    const component = await mountDrawer(mount, {
+      open: false,
+    });
     await expect(component.locator('[data-testid="cart-drawer"]')).not.toBeVisible();
   });
 });
